2ae6053b1cbd2d1a7c759d17787f06b5575ab86f
mtfalls <goldjet45@gmail.com>
2017-05-31 21:54:12 -0400

    scroll to top button added

--- app/app.module.js
+++ app/app.module.js
@@ -1,8 +1,46 @@
 'use strict';
 
-// Define the `phonecatApp` module
-angular.module('Rotom-Dex-Evolved', [
+var app = angular.module('Rotom-Dex-Evolved', [
   'ngRoute',
   'ui.bootstrap',
   'zygardeCounter'
 ]);
+
+app.directive('scrollOnClick', function() {
+  return {
+    restrict: 'EA',
+    template:'<a id="scrolltag" title="Click here to go top" class="scrollup">Scroll</a>',
+    link: function(scope, $elm) {
+      var scrollObject = {};
+      var scrollElement = document.getElementById('scrolltag');
+			window.onscroll = getScrollPosition;
+
+      scrollElement.addEventListener("click", scrollToTop, false);
+
+			function getScrollPosition(){
+        scrollObject = {
+           x: window.pageXOffset,
+           y: window.pageYOffset
+        }
+        if(scrollObject.y > 300) {
+            scrollElement.classList.add("visible");
+        } else {
+            scrollElement.classList.remove("visible");
+        }
+    	}
+
+      function scrollToTop() {
+        var scrollDuration = 500;
+        var scrollStep = -window.scrollY / (scrollDuration / 15);
+        var scrollInterval = setInterval(function(){
+          if (window.scrollY != 0) {
+            window.scrollBy(0, scrollStep);
+          } else {
+            clearInterval(scrollInterval);
+          }
+        },15);
+      }
+
+    }
+  }
+});

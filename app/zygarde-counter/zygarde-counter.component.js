961d345db5043232c1befd83ca0b3a4760192df2
mtfalls <goldjet45@gmail.com>
2017-06-29 18:30:26 -0400

    befre something crazy happens

--- app/zygarde-counter/zygarde-counter.component.js
+++ app/zygarde-counter/zygarde-counter.component.js
@@ -20,5 +20,26 @@ angular.
         modalImg.src = imgSrc;
         captionText.innerHTML = imgAlt;
       };
+      self.collectCell = function collectCell(cellId) {
+        console.log("Collected Cell # " + cellId);
+      };
+      self.showCollected = function updateCollected() {
+        console.log("Here's all of them!");
+        /* Get collectedCells information from localStorage */
+        var cellsCollected = ["Saab", "Volvo", "BMW"];
+        var arrayLength = cellsCollected.length;
+        /* Reset and update div */
+        var div = document.getElementById('collectedCells');
+        div.innerHTML = ''
+        for (var i = 0; i < arrayLength; i++) {
+          /*
+          var content  = document.createTextNode(cellsCollected[i]);
+          div.appendChild(content);*/
+        }
+        /* Show/Hide collected cells <div>
+        var showhide = document.getElementById('collectedCells').style;
+        (showhide.display = showhide.display == "none" ?  "block" : "none" );
+        */
+      };
     }]
   });

1c6fe1d96adb155beef40faa616ac5639ff7361a
mtfalls <goldjet45@gmail.com>
2017-07-06 18:51:11 -0400

    collected cells now displayed in their own table!

--- app/zygarde-counter/zygarde-counter.component.js
+++ app/zygarde-counter/zygarde-counter.component.js
@@ -1,45 +1,117 @@
 'use strict';
-
-// Register `phoneList` component, along with its associated controller and template
+// Register `zygardeCounter` component, along with its associated controller and template
 angular.
   module('zygardeCounter').
   component('zygardeCounter', {
     templateUrl: 'zygarde-counter/zygarde-counter.template.html',
     controller: ['$http', function ($http) {
       var self = this;
-      self.orderProp = "'id'*1";
+
+      /* Initialize webpage, create new localStorage and variable if initial visit */
       $http.get('zygarde-counter/zygarde-cells.json').then(function(response) {
+        /* Parse JSON data */
         self.cells = response.data;
+        var cells = self.cells;
+        var len = self.cells.length;
+
+        /* Start counter at 0 */
+        self.progress = 0;
+
+        /* Check if localStorage exists for client */
+        if (localStorage.getItem("collected") === null) {
+          console.log("Creating local cell tracker");
+          var collected = [];
+
+          /* Start off with no locations collected */
+          for (var i = 0; i < len; i++) {
+            var id = cells[i]['id'];
+            collected[id] = false;
+          }
+
+          /* store cell tracker in local storage */
+          localStorage.setItem("collected", JSON.stringify(collected));
+        }
+
+        /* localStorage exists! Now to continue where the client left off... */
+        else {
+
+          /* Get progress array from "collected" */
+          var collected = JSON.parse(localStorage.getItem("collected"));
+          var progress = 0;
+
+          /* Cell was collected if 'true'*/
+          for (var i = 1; i <= len; i++) {
+            if(collected[i]){
+              progress+=1;
+            }
+          }
+
+          /* Update progress bar */
+          self.progress = progress;
+        }
+
+        /* Show current cell */
+        var curr = document.getElementById('currCell');
+
+        /* Show collected cells */
+        var table = document.getElementById("collectedBody");
+        for (var i = 0; i < len; i++) {
+          // If this cell has already been collected
+          if (collected[i-1]){
+            // Create an empty <tr> element and add it to the 1st position of the table:
+            var row = table.insertRow(-1);
+
+            // Insert new cells (<td> elements) to the "new" <tr> element:
+            var imageCell = row.insertCell(-1);
+            var islandCell = row.insertCell(-1);
+            var locationCell = row.insertCell(-1);
+            var undoCell = row.insertCell(-1);
+
+            // Add relevant content to imageCell
+            var img = document.createElement('img');
+            (function () {
+              var imgSrc = cells[i]["file"];
+              var imgAlt = cells[i]["island"];
+              img.src = imgSrc;
+              img.height = 200;
+              img.width = 200;
+              img.alt = imgAlt;
+              img.addEventListener('click', function () {self.openModalImage(imgSrc, imgAlt);}, false);
+            }());
+            imageCell.appendChild(img);
+
+            // Add relevant content to islandCell
+            islandCell.innerHTML = cells[i]["island"];
+
+            // Add relevant content to locationCell
+            locationCell.innerHTML = cells[i]["location"];
+
+            // Add relevant content to undoCell
+            undoCell.innerHTML = "<i class='glyphicon glyphicon-remove-sign fa-3x' aria-hidden='true'/>";
+            (function () {
+              var id = cells[i]["id"];
+              undoCell.addEventListener('click', function () {self.undoCell(id);}, false);
+            }());
+          } // if
+        } // for (var i = 0; i < len; i++) {
       });
-      self.openModalImage = function openModalImage(imgSrc, imgAlt) {
+      /* Click on image opens it as a modal */
+      self.openModalImage = function (imgSrc, imgAlt) {
         // Get the modal, image, and caption text
         var modal = document.getElementById('myModal');
         var modalImg = document.getElementById("modalImg");
         var captionText = document.getElementById("caption");
+        console.log("imgSrc: " + imgSrc);
+        console.log("imgAlt: " + imgSrc);
         modal.style.display = "block";
         modalImg.src = imgSrc;
         captionText.innerHTML = imgAlt;
       };
-      self.collectCell = function collectCell(cellId) {
+      self.undoCell = function (cellId) {
         console.log("Collected Cell # " + cellId);
       };
-      self.showCollected = function updateCollected() {
+      self.showCollected = function () {
         console.log("Here's all of them!");
-        /* Get collectedCells information from localStorage */
-        var cellsCollected = ["Saab", "Volvo", "BMW"];
-        var arrayLength = cellsCollected.length;
-        /* Reset and update div */
-        var div = document.getElementById('collectedCells');
-        div.innerHTML = ''
-        for (var i = 0; i < arrayLength; i++) {
-          /*
-          var content  = document.createTextNode(cellsCollected[i]);
-          div.appendChild(content);*/
-        }
-        /* Show/Hide collected cells <div>
-        var showhide = document.getElementById('collectedCells').style;
-        (showhide.display = showhide.display == "none" ?  "block" : "none" );
-        */
       };
     }]
   });
